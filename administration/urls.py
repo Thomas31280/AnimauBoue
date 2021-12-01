5dd4e072dcd1f649c17cd055e47433029a12c624
Thomas Duval <tomas1905@hotmail.fr>
2021-12-01 21:23:19 +0100

    First templates and URL paths

--- administration/urls.py
+++ administration/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = "index"),               # On appel la vue index de l'app administration avec ce schéma d'url
+    url(r'^connect_admin_space', views.connect_admin_space, name = "connect_admin_space"),
+    url(r'^update_profile_interface', views.update_profile_interface, name = "update_profile"),
+    url(r'^administration_interface', views.administration_interface, name = "administration_interface"),
 ]
--- administration/views.py
+++ administration/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 def index(request):
     template = loader.get_template('administration/index.html')
     return HttpResponse(template.render(request=request))
+
+def connect_admin_space(request):
+    template = loader.get_template('administration/admin_connect_space.html')
+    return HttpResponse(template.render(request=request))
+
+def update_profile_interface(request):
+    template = loader.get_template('administration/update_profile.html')
+    return HttpResponse(template.render(request=request))
+
+def administration_interface(request):
+    template = loader.get_template('administration/administration_interface.html')
+    return HttpResponse(template.render(request=request))
