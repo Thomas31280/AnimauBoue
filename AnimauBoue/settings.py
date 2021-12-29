f97ac6304eb773b5af283c51b580966a71931745
Thomas Duval <tomas1905@hotmail.fr>
2021-12-29 19:15:05 +0100

    staticfiles

--- AnimauBoue/settings.py
+++ AnimauBoue/settings.py
@@ -15,8 +15,14 @@ from django.contrib.messages import constants as messages
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join('staticfiles')
+STATICFILES_DIRS = (os.path.join('static'),)
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -152,11 +158,11 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    STATIC_ROOT = os.path.join('staticfiles')
 
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
+        os.path.join('static'),
     )
 
     # Simplified static file serving.
---
+++ staticfiles/administration/js/my_scripts.564f81561a23.js
@@ -0,0 +1,47 @@
+//Show or hide new form for a reservation, with update of the "required" parameters of the fields concerned
+function toggleForm(id){
+    var form = document.getElementById(id);
+    var dog_name = document.getElementById('name_'+id);
+    var dog_arrival = document.getElementById('arrival_date_'+id);
+    var dog_departure = document.getElementById('departure_date_'+id);
+    var dog_commentaries = document.getElementById('commentaries_'+id);
+    
+    const html_elements = [dog_name, dog_arrival, dog_departure]
+    
+    if (form.style.display === "none") {
+        form.style.display = "block";
+        
+        //Set required parameter at 'True' for all these elements
+        for (const element of html_elements){
+            element.required = true
+        }
+        
+    } else {
+        form.style.display = "none";
+        //Set required parameter at 'False' for all these elements, and remove the fields content
+        for (var element of html_elements){
+            element.required = false
+            element.value = null
+        }
+        
+        dog_commentaries.value = null
+
+    }
+}
+
+
+//When user has selected a client, the following forms are displayed
+function toggleParkSelection(id, client_name){
+    document.getElementById('client_id').value = id;
+    document.getElementById('clientName').innerHTML = "Vous avez sélectionné le client : " + client_name;
+    document.getElementById('selectedClient').style.display = "block";
+}
+
+
+//When user want to delete something, ask him to confirm his action
+function deleteConfirmation(id){
+    var result = confirm("Attention, la suppression est irréversible ! Confirmez-vous votre choix ?");
+    if (result) {
+        document.getElementById(id).click();
+    }
+}
