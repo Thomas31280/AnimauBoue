8541b1825ab2ec03c2d195b0513feeaffbe28702
Thomas Duval <tomas1905@hotmail.fr>
2021-12-30 15:15:25 +0100

    try to fix relation doesn't exist 3

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from user.views import ConsultAvailability
+#from user.views import ConsultAvailability
 
 urlpatterns = [
-    url(r'^consult_availability', ConsultAvailability.as_view(), name="consult_availability"),
+    #url(r'^consult_availability', ConsultAvailability.as_view(), name="consult_availability"),
 ]
