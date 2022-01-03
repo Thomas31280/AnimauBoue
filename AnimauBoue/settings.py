e209b0a894eddd4115456635261de3cf7eee68e5
Thomas Duval <tomas1905@hotmail.fr>
2022-01-03 19:46:10 +0100

    new statics + sentry

--- AnimauBoue/settings.py
+++ AnimauBoue/settings.py
@@ -168,3 +168,20 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://6030e566f028456b807052becafae1ab@o1070722.ingest.sentry.io/6132998",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
