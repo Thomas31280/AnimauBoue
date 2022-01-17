c662221fe00b44fa34b460574528448d90cf66a4
Thomas Duval <tomas1905@hotmail.fr>
2022-01-17 18:37:41 +0100

    Tests ( 83% )

--- administration/tests.py
+++ administration/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+import datetime
 
 from django.contrib.auth.models import User
 from administration.models import Clients, Dogs, Parks, Reservations
@@ -16,26 +17,26 @@ class AdministrationTests(TestCase):
         Parks.objects.create(name='E1', availability=True).save()
         Parks.objects.create(name='E2', availability=True).save()
         Parks.objects.create(name='E3', availability=True).save()
-        park_1 = Parks.objects.get(name='E1')
-        park_2 = Parks.objects.get(name='E2')
-        park_3 = Parks.objects.get(name='E3')
+        self.park_1 = Parks.objects.get(name='E1')
+        self.park_2 = Parks.objects.get(name='E2')
+        self.park_3 = Parks.objects.get(name='E3')
         
         Clients.objects.create(first_name='first_name_1', name='name_1', phone='+33602157836').save()
         Clients.objects.create(first_name='first_name_2', name='name_2', phone='+33602157837').save()
         Clients.objects.create(first_name='first_name_3', name='name_3', phone='+33602157838').save()
-        client_1 = Clients.objects.get(first_name='first_name_1')
-        client_2 = Clients.objects.get(first_name='first_name_2')
-        client_3 = Clients.objects.get(first_name='first_name_3')
+        self.client_1 = Clients.objects.get(first_name='first_name_1')
+        self.client_2 = Clients.objects.get(first_name='first_name_2')
+        self.client_3 = Clients.objects.get(first_name='first_name_3')
 
-        Dogs.objects.create(name='name_1', owner=client_1).save()
-        Dogs.objects.create(name='name_2', owner=client_2).save()
-        Dogs.objects.create(name='name_3', owner=client_3).save()
-        dog_1 = Dogs.objects.get(name='name_1')
-        dog_2 = Dogs.objects.get(name='name_2')
-        dog_3 = Dogs.objects.get(name='name_3')
+        Dogs.objects.create(name='name_1', owner=self.client_1).save()
+        Dogs.objects.create(name='name_2', owner=self.client_2).save()
+        Dogs.objects.create(name='name_3', owner=self.client_3).save()
+        self.dog_1 = Dogs.objects.get(name='name_1')
+        self.dog_2 = Dogs.objects.get(name='name_2')
+        self.dog_3 = Dogs.objects.get(name='name_3')
 
-
-
+        Reservations.objects.create(client=self.client_1, park=self.park_1, price=140, dog_1=self.dog_1, dog_1_arrival=datetime.datetime(2022, 1, 10, 12, 15), dog_1_departure=datetime.datetime(2022, 2, 10, 12, 15), dog_2=None, dog_2_arrival=None, dog_2_departure=None, dog_3=None, dog_3_arrival=None, dog_3_departure=None, dog_4=None, dog_4_arrival=None, dog_4_departure=None, dog_5=None, dog_5_arrival=None, dog_5_departure=None)
+        self.reservation_1 = Reservations.objects.get(price=140)
 
     # test that index returns a 200
     def test_index(self):
@@ -137,10 +138,8 @@ class AdministrationTests(TestCase):
     def test_UpdateClient_get(self):
         c = Client()
         c.login(username='user', password='password')
-        Clients.objects.create(first_name='first_name', name='name', phone='+33602157847').save()
-        client_id = Clients.objects.get(name='name').id
         
-        datas = {'client': client_id, 'client_phone': '+33602157847'}
+        datas = {'client': self.client_1.id, 'client_phone': str(self.client_1.phone)}
 
         response = c.get('/update_client/', datas)
         self.assertEqual(response.status_code, 200)
@@ -149,10 +148,8 @@ class AdministrationTests(TestCase):
     def test_UpdateClient_post(self):
         c = Client()
         c.login(username='user', password='password')
-        client_id = Clients.objects.get(name='name_1').id
-        client_phone = Clients.objects.get(name='name_1').phone
         
-        datas = {'firstName': 'first_name', 'phone': '+33613008748', 'email': '', 'client_id': client_id, 'current_phone': client_phone}
+        datas = {'firstName': 'first_name', 'phone': '+33613008748', 'email': '', 'client_id': self.client_1.id, 'current_phone': self.client_1.phone}
 
         response = c.post('/update_client/', datas)
         self.assertEqual(response.status_code, 302)
@@ -161,12 +158,188 @@ class AdministrationTests(TestCase):
     def test_client_reservations(self):
         c = Client()
         c.login(username='user', password='password')
-        Clients.objects.create(first_name='first_name', name='name', phone='+33602157847').save()
-        client_id = Clients.objects.get(name='name').id
         
-        datas = {'client': client_id, 'client_phone': '+33602157847'}
+        datas = {'client': self.client_1.id, 'client_phone': str(self.client_1.phone)}
 
         response = c.get('/client_reservations/', datas)
         self.assertEqual(response.status_code, 200)
+
+    # test that a post request on delete_client returns a 302
+    def test_delete_client(self):
+        c = Client()
+        c.login(username='user', password='password')
+        
+        datas = {'client_id': self.client_1.id, 'client_phone': str(self.client_1.phone)}
+
+        response = c.post('/delete_client/', datas)
+        self.assertEqual(response.status_code, 302)
+
+    # test that a post request on clients_profiles returns a 200
+    def test_clients_profiles(self):
+        c = Client()
+        c.login(username='user', password='password')
+        
+        datas = {'recherche_client': 'name'}
+
+        response = c.post('/clients_profiles_client/', datas)
+        self.assertEqual(response.status_code, 200)
     
+    # test that a get request on delete_client returns a 302
+    def test_delete_client(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.post('/delete_client/', {'client_id': self.client_2.id, 'client_phone': str(self.client_2.phone)})
+        self.assertEqual(response.status_code, 302)
     
+    # test that a get request on client returns a 200
+    def test_client(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.get('/client/', {'client': self.client_1.id})
+        self.assertEqual(response.status_code, 200)
+    
+    # test that a get request on client returns a 302 if client doesn't exist
+    def test_client_fail(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.get('/client/', {'client': ''})
+        self.assertEqual(response.status_code, 302)
+    
+    # test that a get request on ReservationForm returns a 200
+    def test_ReservationForm_get(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.get('/reservation_form/')
+        self.assertEqual(response.status_code, 200)
+    
+    # test that a post request on ReservationForm returns a 200
+    def test_ReservationForm_post(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.post('/reservation_form/', {'recherche_client': 'name'})
+        self.assertEqual(response.status_code, 200)
+    
+    # test that a post request on delete_reservation returns a 302
+    def test_delete_reservation_post(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        datas = {'reservation_id': self.reservation_1.id, 'client': self.reservation_1.client.id}
+
+        response = c.post('/delete_reservation/', datas)
+        self.assertEqual(response.status_code, 302)
+
+    # test that a get request on ArrivalAndDeparture returns a 200
+    def test_ArrivalAndDeparture_get(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.get('/arrival-departure_interface/')
+        self.assertEqual(response.status_code, 200)
+    
+    # test that a post request on ArrivalAndDeparture returns a 200
+    def test_ArrivalAndDeparture_post(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        datas = {'month': 1, 'year': 2022}
+
+        response = c.post('/arrival-departure_interface/', datas)
+        self.assertEqual(response.status_code, 200)
+    
+    # test that a get request on Stats returns a 200
+    def test_Stats_get(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.get('/stats/')
+        self.assertEqual(response.status_code, 200)
+    
+    # test that a post request on Stats returns a 200
+    def test_Stats_post(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        datas = {'month': 1, 'year': 2022}
+
+        response = c.post('/stats/', datas)
+        self.assertEqual(response.status_code, 200)
+    
+    # test that a get request on NewDog returns a 200
+    def test_NewDog_get(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.get('/add_dog/', {'client': self.client_1.id, 'client_phone': str(self.client_1.phone)})
+        self.assertEqual(response.status_code, 200)
+    
+    # test that a post request on NewDog returns a 302
+    def test_NewDog_post(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        datas = {'client_id': self.client_1.id, 'client_phone': str(self.client_1.phone), 'name': 'name', 'transponder': 874521201254862}
+
+        response = c.post('/add_dog/', datas)
+        self.assertEqual(response.status_code, 302)
+    
+    # test that a post request on NewDog returns a 302 if bad transponder
+    def test_NewDog_bad_transponder(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        datas = {'client_id': self.client_1.id, 'client_phone': str(self.client_1.phone), 'name': 'name', 'transponder': '874521201254862'}
+
+        response = c.post('/add_dog/', datas)
+        self.assertEqual(response.status_code, 302)
+    
+    # test that a post request on NewDog returns a 302 if client.phone != client_phone
+    def test_NewDog_bad_client_phone(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        datas = {'client_id': self.client_1.id, 'client_phone': str(self.client_2.phone), 'name': 'name', 'transponder': 874521201254862}
+
+        response = c.post('/add_dog/', datas)
+        self.assertEqual(response.status_code, 302)
+    
+    # test that a get request on UpdateDog returns a 200
+    def test_UpdateDog_get(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.get('/update_dog/', {'dog': self.dog_1.id, 'owner': self.dog_1.owner.id})
+        self.assertEqual(response.status_code, 200)
+    
+    # test that a post request on UpdateDog returns a 302
+    def test_UpdateDog_post(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        datas = {'dog_id': self.dog_1.id, 'owner_id': self.dog_1.owner.id, 'name': 'new_name', 'transponder': 874521521754862}
+
+        response = c.post('/update_dog/', datas)
+        self.assertEqual(response.status_code, 302)
+    
+    # test that a post request on delete_dog returns a 302
+    def test_delete_dog_post(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        datas = {'dog_id': self.dog_1.id, 'owner': self.dog_1.owner.id}
+
+        response = c.post('/delete_dog/', datas)
+        self.assertEqual(response.status_code, 302)
+
+    # test that a get request on UpdateReservation returns a 200
+    def test_UpdateReservation_get(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.get('/update_reservation/', {'reservation': self.reservation_1.id, 'client_phone': str(self.reservation_1.client.phone)})
+        self.assertEqual(response.status_code, 200)
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,56 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import datetime
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from administration.models import Clients, Dogs, Parks, Reservations
+
+############################
+#########Unit Tests#########
+############################
+
+class UserTests(TestCase):
+    
+    def setUp(self):
+        User.objects.create_superuser('user', 'user@mail.com', 'password').save()
+        
+        Parks.objects.create(name='E1', availability=True).save()
+        Parks.objects.create(name='E2', availability=True).save()
+        Parks.objects.create(name='E3', availability=True).save()
+        self.park_1 = Parks.objects.get(name='E1')
+        self.park_2 = Parks.objects.get(name='E2')
+        self.park_3 = Parks.objects.get(name='E3')
+        
+        Clients.objects.create(first_name='first_name_1', name='name_1', phone='+33602157836').save()
+        Clients.objects.create(first_name='first_name_2', name='name_2', phone='+33602157837').save()
+        Clients.objects.create(first_name='first_name_3', name='name_3', phone='+33602157838').save()
+        self.client_1 = Clients.objects.get(first_name='first_name_1')
+        self.client_2 = Clients.objects.get(first_name='first_name_2')
+        self.client_3 = Clients.objects.get(first_name='first_name_3')
+
+        Dogs.objects.create(name='name_1', owner=self.client_1).save()
+        Dogs.objects.create(name='name_2', owner=self.client_2).save()
+        Dogs.objects.create(name='name_3', owner=self.client_3).save()
+        self.dog_1 = Dogs.objects.get(name='name_1')
+        self.dog_2 = Dogs.objects.get(name='name_2')
+        self.dog_3 = Dogs.objects.get(name='name_3')
+
+        Reservations.objects.create(client=self.client_1, park=self.park_1, price=140, dog_1=self.dog_1, dog_1_arrival=datetime.datetime(2022, 1, 10), dog_1_departure=datetime.datetime(2022, 2, 10), dog_2=None, dog_2_arrival=None, dog_2_departure=None, dog_3=None, dog_3_arrival=None, dog_3_departure=None, dog_4=None, dog_4_arrival=None, dog_4_departure=None, dog_5=None, dog_5_arrival=None, dog_5_departure=None)
+        self.reservation_1 = Reservations.objects.get(price=140)
+
+    # test that a get request on ConsultAvailability returns a 200
+    def test_ConsultAvailability_get(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        response = c.get('/consult_availability/')
+        self.assertEqual(response.status_code, 200)
+
+    # test that a get request on ConnectAdminSpace returns a 200
+    def test_ConsultAvailability_post(self):
+        c = Client()
+        c.login(username='user', password='password')
+
+        datas = {'month': 1, 'year': 2022}
+
+        response = c.post('/consult_availability/', datas)
+        self.assertEqual(response.status_code, 200)
